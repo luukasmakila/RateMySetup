e90cc0b934b39602d1f5afe09ac716bd6bbb95c8
Luukas <luukas.makila@kolumbus.fi>
2022-01-28 16:46:23 +0200

    cleaning up the files

--- frontend/src/components/Imgbody.js
+++ frontend/src/components/Imgbody.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Imgbody = () => {
   return (
     <div className='showcase'>
-      <h2>Hello</h2>
-      <h1>Hello world</h1>
-      <p>Rate other's or create a post of your own!</p>
+      <h2></h2>
+      <h1>Rate My Setup</h1>
+      <p>Find inspiration from other's setups or inspire other's by creating a post of your own!</p>
       <div className='showcase__buttons'>
         <button className='showcase__buttons-posts'>Review other's!</button>
         <button className='showcase__buttons-create'>Create post!</button>
