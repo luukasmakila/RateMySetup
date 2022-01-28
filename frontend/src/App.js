ab752ab934e0cc77d67b25cecec3df2e88124138
Luukas <luukas.makila@kolumbus.fi>
2022-01-28 12:07:48 +0200

    created components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Imgbody from './components/Imgbody'
 
 const App = () => {
   return (
     <div>
-      Hello
+      <Header/>
+      <Imgbody/>
+      {/* Routes here */}
+      <Login/>
+      <SignUp/>
+      <Footer/>
     </div>
   )
 }
---
+++ frontend/src/components/Imgbody.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Imgbody = () => {
+  return (
+    <div>Main page picture with a summary of the page</div>
+  )
+}
+
+export default Imgbody
