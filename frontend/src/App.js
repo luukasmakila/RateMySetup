c5a53b90bb189bb1e9030a6ab3294ecddcc344ec
Luukas <luukas.makila@kolumbus.fi>
2022-01-28 18:03:46 +0200

    Added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/Footer'
+import Home from './components/Home'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-import Imgbody from './components/Imgbody'
 
 const App = () => {
   return (
     <div>
       <Header/>
-      <div className='main'>
-        <Imgbody/>
-      </div>
-      {/* Routes here */}
-      <Footer/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Imgbody from './Imgbody'
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='main'>
+          <Imgbody/>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
