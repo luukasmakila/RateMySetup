5d51df920d0e61a828eef0aa740c4bdf979c2db7
Luukas <luukas.makila@kolumbus.fi>
2022-02-05 17:06:39 +0200

    working on picture upload method

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
+import FileBase64 from 'react-file-base64'
 
 const CreatePost = () => {
   const [bio, setBio] = useState('')
+  const [image, setImage] = useState('')
 
-  const handleFileSelected = event => {
-    console.log(event)
+  const handleSubmit = () => {
+    console.log(image)
   }
 
   return (
     <div>
-      <form className='setup-info'>
+      <form className='setup-info' onSubmit={handleSubmit}>
         <h2>Setup info</h2>
         <label>Setup parts</label>
         <input type='text' value={bio} onChange={(e) => setBio(e.target.value)}/>
-        <label>Image</label>
-        <input type='file' onChange={handleFileSelected}/>
+        <label>Setup picture</label>
+        <FileBase64
+          multiple={ false }
+          onDone={({base64}) => setImage(base64) }
+        />
+        <button type='submit'>Submit</button>
       </form>
+      <div className='create-post-image'>
+        <img style={{ width: 600, height: 300 }} src={image}/>
+      </div>
     </div>
   )
 }
