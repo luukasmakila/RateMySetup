76bfacd50a93581ceab23ae52d337f90ebffff08
Luukas <luukas.makila@kolumbus.fi>
2022-04-28 02:47:27 +0300

    added cors everywhere proxy for https

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -20,7 +20,7 @@ const SignUp = () => {
       password
     }
 
-    const result = await axios.post('http://ec2-35-173-188-150.compute-1.amazonaws.com:3001/api/auth/sign-up', newUser)
+    const result = await axios.post('https://cors-everywhere-me.herokuapp.com/http://ec2-35-173-188-150.compute-1.amazonaws.com:3001/api/auth/sign-up', newUser)
     const token = result.data.token
     const userId = result.data.id
     localStorage.setItem('authToken', token)
